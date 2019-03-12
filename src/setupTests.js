3c9de9d1ae77dedbcdde290a63e348059ebd4ca4
Nicholas Brun <nicholasb@hotmail.fr>
2019-03-12 16:13:41 +0100

    Fix audit and remove tests

--- src/setupTests.js
+++
@@ -1,8 +0,0 @@
-import chai from 'chai'
-import dirtyChai from 'dirty-chai'
-import createChaiEnzyme from 'chai-enzyme'
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-chai.use(dirtyChai).use(createChaiEnzyme());
-Enzyme.configure({ adapter: new Adapter() });
