2a7b9ca4e3b54526264b2d95274b367d2cd56828
Nicholas Brun <nicholasb@hotmail.fr>
2019-03-13 11:55:05 +0100

    Add pages for connexion and profile

---
+++ src/js/views/connexion/connexion-mail/ConnexionMail.jsx
@@ -0,0 +1,46 @@
+//Imports bibliothèques
+import React, { Component, Fragment } from "react";
+
+//Imports des composants
+import Text from "../../components/text/Text.jsx";
+
+//Imports de redux
+import { connect } from "react-redux";
+import { changeLocation } from "../../redux/actions.js";
+
+//Imports des styles
+import "./ConnexionMail.scss";
+
+//------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------
+
+class ConnexionMail extends Component {
+  constructor(props) {
+    super(props);
+
+    var currentLocation = this.props.location.pathname;
+    this.props.changeLocation(currentLocation);
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Text textContent={this.props.location.pathname} fontSize="25" isBold />
+      </Fragment>
+    );
+  }
+}
+
+//------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------
+
+const mapDispatchToProps = dispatch => {
+  return {
+    changeLocation: location => dispatch(changeLocation(location))
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(ConnexionMail);
